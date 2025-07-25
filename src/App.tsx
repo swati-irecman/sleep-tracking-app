e9c8adbbbeebb613f34e0e0f7ce1406456163f31
KanchanWadhai <kanchanwadhai2003@gmail.com>
2025-07-25 22:37:35 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,3 @@
-// import React from 'react';
-// import './App.css';
-// import SleepDashboard from './components/SleepDashboard';
-
-// const App: React.FC = () => {
-//   return (
-//     <>
-//       <header className="header">Sleep Analysis Dashboard</header>
-//       <main className="main">
-//         <SleepDashboard />
-//       </main>
-//     </>
-//   );
-// };
-
-// export default App;
-
-
-// App.tsx
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
